4389fff8a0a822c75b163d07173b78e95fa72d3d
RaminFerdos <ferdos.ramin@gmail.com>
2022-08-14 23:28:05 +0430

    initilizing the prject

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#0d0d0d',
+        'dark-high': '#262626',
+        'dark-low': '#404040',
+        'light-high': '#737373',
+        'light-low': '#a6a6a6',
+        'red-high': '#b02929',
+        'red-low': '#da5656',
+        'green': '#29b067'
+      },
+      fontFamily: {
+        'sf': ["'SF Pro Text'"]
+      }
+    },
+  },
+}
