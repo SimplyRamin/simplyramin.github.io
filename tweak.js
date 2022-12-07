41ba6c1732545e901144d988be4126e615d4c552
ferdos.ramin@gmail.com <ferdos.ramin@gmail.com>
2022-12-07 11:33:59 +0330

    making hamburger menu working

--- tweak.js
+++ tweak.js
@@ -8,6 +8,19 @@ function updateList() {
 	document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
 }
 
+function openNav() {
+	document.getElementById('side-nav').style.width = '50%';
+	document.getElementById('main').classList.add('bg-blend-darken');
+	document.getElementById('main').classList.remove('bg-body');
+
+}
+
+function closeNav() {
+	document.getElementById('side-nav').style.width = '0px';
+	document.getElementById('main').classList.add('bg-body');
+	document.getElementById('main').classList.remove('bg-blend-darken');
+}
+
 updateList();
 window.addEventListener('scroll', () => {
     updateList();
